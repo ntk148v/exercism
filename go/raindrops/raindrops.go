835cc6c9e443d3381774e82fe3518dac66b81557
Kien Nguyen <kiennt65@viettel.com.vn>
2018-11-08 09:30:22 +0700

    Update raindrops solution

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,12 +1,12 @@
-/*Package raindrops converts a number to string,
-the contents of which depend on the number's factors.*/
+// Package raindrops converts a number to string
+// the contents of which depend on the number's factors.
 package raindrops
 
 import (
 	"strconv"
 )
 
-// Convert a number to string
+// Convert converts a number to string
 func Convert(number int) (sound string) {
 	if number%3 == 0 {
 		sound += "Pling"
