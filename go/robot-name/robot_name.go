687f5e6d95c6f691daa4ffdce26d2a8e226fbb94
Kien Nguyen <kiennt65@viettel.com.vn>
2018-12-29 23:05:23 +0700

    Update solution robot-name

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -16,12 +16,12 @@ var usedNames = make(map[string]bool)
 
 // Name defines the random unique name of robot.
 func (r *Robot) Name() string {
-	rand.Seed(time.Now().UTC().UnixNano())
+	ra := rand.New(rand.NewSource(time.Now().Unix()))
 	for inUse, ok := r.name == "", true; ok && inUse; inUse, ok = usedNames[r.name] {
 		r.name = fmt.Sprintf("%c%c%03d",
-			byte(65+rand.Intn(26)),
-			byte(65+rand.Intn(26)),
-			rand.Intn(1000))
+			byte(65+ra.Intn(26)),
+			byte(65+ra.Intn(26)),
+			ra.Intn(1000))
 	}
 	usedNames[r.name] = true
 	return r.name
