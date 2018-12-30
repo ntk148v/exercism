8afb235e3633b2c6d0979f8ffbd4ad979c3afeba
Kien Nguyen <kiennt65@viettel.com.vn>
2018-12-30 10:03:52 +0700

    Update robot-name solution

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -1,6 +1,7 @@
 package robotname
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
 	"time"
@@ -15,16 +16,26 @@ type Robot struct {
 var usedNames = make(map[string]bool)
 
 // Name defines the random unique name of robot.
-func (r *Robot) Name() string {
+func (r *Robot) Name() (string, error) {
+	if r.name > "" {
+		return r.name, nil
+	}
 	ra := rand.New(rand.NewSource(time.Now().Unix()))
+	first := fmt.Sprintf("%c%c%03d",
+		byte(65+ra.Intn(26)),
+		byte(65+ra.Intn(26)),
+		ra.Intn(1000))
 	for inUse, ok := r.name == "", true; ok && inUse; inUse, ok = usedNames[r.name] {
 		r.name = fmt.Sprintf("%c%c%03d",
 			byte(65+ra.Intn(26)),
 			byte(65+ra.Intn(26)),
 			ra.Intn(1000))
+		if r.name == first {
+			return "", errors.New("All valid robot names are used")
+		}
 	}
 	usedNames[r.name] = true
-	return r.name
+	return r.name, nil
 }
 
 // Reset resets a given robot's name.
